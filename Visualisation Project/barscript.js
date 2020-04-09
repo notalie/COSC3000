8eb3e11b7399c0c46025197e4f468dfef60593c7
Zenzlia <natalie.hong@uqconnect.edu.au>
2020-04-09 19:24:56 +1000

    Added techniques section, improved line graph, added scatterplots

--- Visualisation Project/barscript.js
+++ Visualisation Project/barscript.js
@@ -9,8 +9,7 @@ function drawBarChart(stateData, presets, chartTitle, iconsOn) {
             charNames.push(stateData[i][0]);
             stateData[i][0] = "";
         }
-    }
-
+	}
 
     google.charts.load("current", {packages:["corechart"]});
     google.charts.setOnLoadCallback(drawChart);
@@ -61,6 +60,73 @@ function Comparator(a, b) {
 var chosenState;
 var chosenQuarter = "Q1"
 var icons = true;
+
+var QUARTERS = ["Q1", "Q2", "Q3", "Q4"]
+
+function showTotalPlayers() {
+
+	var overallData = [];
+	overallData.push(["State", "Players", { role: "style" } ]);
+
+	for (var i = 0; i < stateList.length; i++) {
+		var playerList = []
+		var stateInfo = []
+		var currentState = stateList[i];
+        for (var j = 0; j < QUARTERS.length; j++) {
+			var currentQuarter = QUARTERS[j];
+            for (var k = 0; k < CHARACTERS.length; k++) {
+                var currentChar = CHARACTERS[k];
+				
+				if (stateData[currentQuarter][currentState][currentChar] !== undefined) {
+                    for (var o = 0; o < stateData[currentQuarter][currentState][currentChar][0].length; o++) {
+                        if (!playerList.includes(stateData[currentQuarter][currentState][currentChar][0][o])) {
+                            playerList.push(stateData[currentQuarter][currentState][currentChar][0][o])
+                        }
+                    }
+                }
+			}
+		}
+		stateInfo.push(currentState, playerList.length, STATE_COLOURS[currentState]);
+		overallData.push(stateInfo);
+		
+	}
+
+	overallData = overallData.sort(Comparator);
+
+	google.charts.load("current", {packages:["corechart"]});
+    google.charts.setOnLoadCallback(drawChart);
+	function drawChart() {
+		var data = google.visualization.arrayToDataTable(
+			overallData
+		);
+
+		var view = new google.visualization.DataView(data);
+		view.setColumns([0, 1,
+						{ calc: "stringify",
+							sourceColumn: 1,
+							type: "string",
+							role: "annotation" },
+						2]);
+
+		var options = {
+			title: "The Amount of Total Registered Players in Each State",
+			width: 1800,
+			height: 800,
+			bar: {groupWidth: "80%"},
+			legend: { position: "none" },
+			vAxis: {
+				title: 'States'
+			},
+			hAxis: {
+				title: 'Amount of Unique Players'
+			}
+		};
+
+		var chart = new google.visualization.BarChart(document.getElementById("player-chart"));
+		chart.draw(view, options);
+	}
+}
+
 
 var stateList = ["QLD", "NSW", "TAS", "VIC", "SA", "ACT", "NZ", "WA", "INT"];
 // Redraw the Bar Graph 
@@ -149,7 +215,7 @@ $(document).ready(function() {
 		
         barGraphClick(chosenState)
     });
-
+	showTotalPlayers()
 });
 
 
--- Visualisation Project/scatterScript.js
+++ Visualisation Project/scatterScript.js
@@ -1,32 +1,96 @@
-function drawIt() {
+function drawIt(dataToUse, location, chartTitle) {
+    dataToUse.unshift(['ID', 'Amount of Registered Players', 'Elo Gain', 'Region',     'Radius']);
     google.charts.load('current', {'packages':['corechart']});
     google.charts.setOnLoadCallback(drawChart);
     function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-        ['Age', 'Weight'],
-        [ 8,      12],
-        [ 4,      5.5],
-        [ 11,     14],
-        [ 4,      5],
-        [ 3,      3.5],
-        [ 6.5,    7]
-        ]);
-
-        var options = {
-        title: 'Age vs. Weight comparison',
-        hAxis: {title: 'Age', minValue: 0, maxValue: 15},
-        vAxis: {title: 'Weight', minValue: 0, maxValue: 15},
-        legend: 'none'
+        var data = google.visualization.arrayToDataTable(
+            dataToUse
+        );
+    
+          var options = {
+            title: chartTitle,
+            hAxis: {title: 'Amount of Registered Players', maxValue:300},
+            vAxis: {title: 'Elo Gain', maxValue:150000},
+            bubble: {textStyle: {fontSize: 11}},
+            explorer: { keepInBounds: true }
         };
-
-        var chart = new google.visualization.ScatterChart(document.getElementById('main-chart'));
-
-        chart.draw(data, options);
+    
+          var chart = new google.visualization.BubbleChart(document.getElementById(location));
+          chart.draw(data, options);
     }
 }
 
 
+function sortData() {
+    var overallData = [];
+    var QUARTERS = ["Q1", "Q2", "Q3", "Q4"];
+    var stateList = ["QLD", "NSW", "VIC", "SA", "ACT", "NZ", "WA"];
+    for (var i = 0; i < QUARTERS.length; i++) {
+        var currentQuarter = QUARTERS[i];
+        for (var j = 0; j < stateList.length; j++) {
+            var currentState = stateList[j];
+            var quarterData = []
+            var eloGain = 0
+            var playerList = [];
+            var totalPlayers = 0;
+            for (var k = 0; k < CHARACTERS.length; k++) {
+                var currentChar = CHARACTERS[k];
+                if (stateData[currentQuarter][currentState][currentChar] !== undefined) {
+                    eloGain += stateData[currentQuarter][currentState][currentChar][1];
+                    for (var o = 0; o < stateData[currentQuarter][currentState][currentChar][0].length; o++) {
+                        if (!playerList.includes(stateData[currentQuarter][currentState][currentChar][0][o])) {
+                            playerList.push(stateData[currentQuarter][currentState][currentChar][0][o])
+                        }
+                    }
+                }
+            }
+            quarterData.push(stateList[j], playerList.length, eloGain, QUARTERS[i], 1);
+            overallData.push(quarterData);
+        }
+    }
+    return overallData;
+}
 
+
+function sortData2() {
+    var overallData = [];
+    var QUARTERS = ["Q1", "Q2", "Q3", "Q4"];
+    var stateList = ["QLD", "NSW", "VIC", "SA", "ACT", "NZ", "WA"];
+    for (var i = 0; i < QUARTERS.length; i++) {
+        var currentQuarter = QUARTERS[i];
+        for (var j = 0; j < stateList.length; j++) {
+            var currentState = stateList[j];
+            var quarterData = []
+            var eloGain = 0
+            var totalPlayers = 0;
+            var playerList = []
+            for (var k = 0; k < CHARACTERS.length; k++) {
+                var currentChar = CHARACTERS[k];
+                if (stateData[currentQuarter][currentState][currentChar] !== undefined) {
+                    eloGain += stateData[currentQuarter][currentState][currentChar][1];
+                    for (var o = 0; o < stateData[currentQuarter][currentState][currentChar][0].length; o++) {
+                        if (!playerList.includes(stateData[currentQuarter][currentState][currentChar][0][o])) {
+                            playerList.push(stateData[currentQuarter][currentState][currentChar][0][o])
+                        }
+                    }
+                }
+            }
+            quarterData.push(stateList[j], playerList.length, eloGain, "A", 1);
+            overallData.push(quarterData);
+        }
+    }
+    return overallData;
+}
+//ID', 'Amount of Unique Players', 'Elo Gain', 'Region',     'Radius']);
 $(document).ready(function() {
-    drawIt();
+    var data = sortData();
+    data.push(['QLD', 0,0,"Q4", 2])
+    drawIt(data, 'main-chart', 'Correlation Between the Amount of Registered Players in the State and the Amount of Elo Gained per Quarter');
+
+    data = sortData();
+    drawIt(data, 'main-chart1.5', 'Correlation Between the Amount of Registered Players in the State and the Amount of Elo Gained per Quarter')
+
+    var data2 = sortData2();
+    data2.push(['QLD', 0,0,"A", 2])
+    drawIt(data2, 'main-chart2', 'Correlation Between the Amount of Registered Players and the Amount of Elo Gained for the whole of Australia');
 });
