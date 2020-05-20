f44620849b2a050f8144efc1fcc53d3cfbe3ff5a
Zenzlia <natalie.hong@uqconnect.edu.au>
2020-05-20 15:34:44 +1000

    Doco some stuff

--- Graphics Project/mega_racer/terrain.py
+++ Graphics Project/mega_racer/terrain.py
@@ -257,9 +257,9 @@ class Terrain:
             void main() 
             {
                 vec2 testvecw = v2f_worldSpacePosition.xy;
-                //https://stackoverflow.com/questions/41984724/calculating-angle-between-two-vectors-in-glsl
-                vec3 worldSpace_Normal = normalize(v2f_worldSpaceNormal);
-                vec3 zaxis_Normal = normalize(vec3(0, 0, 1));
+                // Help from: https://stackoverflow.com/questions/41984724/calculating-angle-between-two-vectors-in-glsl
+                vec3 worldSpace_Normal = normalize(v2f_worldSpaceNormal); // Normal of world space
+                vec3 zaxis_Normal = normalize(vec3(0, 0, 1)); // Normal of Z-axis
                 float angle =  acos(dot(worldSpace_Normal, zaxis_Normal));
                 vec3 materialColour = vec3(v2f_height/terrainHeightScale);
                 
@@ -285,7 +285,7 @@ class Terrain:
                 vec3 reflectedLight = computeShading(materialColour, v2f_viewSpacePosition, v2f_viewSpaceNormal, viewSpaceLightPosition, sunLightColour);
 	            fragmentColor = vec4(toSrgb(reflectedLight), 1.0);
 	            //fragmentColor = vec4(toSrgb(vec3(v2f_height/terrainHeightScale)), 1.0);
-	            // Apply fog
+	            // Applying fog
 	            fragmentColor = vec4(toSrgb(applyFog(reflectedLight, -v2f_viewSpacePosition.z)), 1.0);
             }
 """
